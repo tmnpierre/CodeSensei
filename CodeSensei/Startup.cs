6354d662ef2e28d07aa4e8669aa3df45421dd4af
tmnpierre <tomyn.pierre@gmail.com>
2024-01-22 20:03:42 +0100

    feat: integrate repository pattern and update database configurations

--- CodeSensei/Startup.cs
+++ CodeSensei/Startup.cs
@@ -1,16 +1,13 @@
 ﻿using CodeSensei.Bots.Interfaces;
-using CodeSensei.Bots.Utilities;
 using CodeSensei.Data.Contexts;
 using CodeSensei.Services;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Bot.Builder;
 using CodeSenseiChatbot.Adapters;
+using CodeSensei.Data.Repositories.Implementations;
+using CodeSensei.Data.Repositories.Interfaces;
+using CodeSensei.Data.Models;
 
 public class Startup
 {
@@ -29,6 +26,9 @@ public class Startup
         services.AddTransient<IBot, CodeSensei.Bots.Utilities.CodeSenseiChatbot>();
         services.AddDbContext<FeedbackContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("FeedbackDatabase")));
+        services.AddDbContext<FeedbackContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("FeedbackDatabase")));
+        services.AddScoped<IRepository<FeedbackRecord>, FeedbackRepository>();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
