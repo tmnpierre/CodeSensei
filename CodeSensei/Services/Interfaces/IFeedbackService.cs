0099abea1a9a0277f383ab5512189abeaa40610b
tmnpierre <tomyn.pierre@gmail.com>
2024-01-25 18:02:43 +0100

    refactor: reorganize project structure and update naming conventions

--- CodeSensei/Services/Interfaces/IFeedbackService.cs
+++ CodeSensei/Services/Interfaces/IFeedbackService.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace CodeSensei.Bots.Interfaces
 {
-    public interface IFeedbackManager
+    public interface IFeedbackService
     {
         Task RequestFeedbackAsync(ITurnContext turnContext, CancellationToken cancellationToken);
         Task HandleFeedbackAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken);
